a0ee0c278b23dc13ab84527f60048575866652bc
stnqq <life33890@gmail.com>
2026-05-24 14:59:23 +0500

    task is done with vulnerability with film rents

--- fourteen_collections/film_task/DateTime.java
+++ fourteen_collections/film_task/DateTime.java
@@ -1,4 +1,32 @@
 package fourteen_collections.film_task;
 
 public class DateTime {
+    public final int day;
+    public final int month;
+    public final int year;
+
+    public final int hours;
+    public final int minutes;
+    public final int seconds;
+
+    public DateTime(int day, int month, int year, int hours, int minutes, int seconds) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+    }
+
+    @Override
+    public String toString() {
+        return "DateTime{" +
+                "day=" + day +
+                ", month=" + month +
+                ", year=" + year +
+                ", hours=" + hours +
+                ", minutes=" + minutes +
+                ", seconds=" + seconds +
+                '}';
+    }
 }
--- fourteen_collections/film_task/Practice.java
+++ fourteen_collections/film_task/Practice.java
@@ -1,4 +1,27 @@
 package fourteen_collections.film_task;
 
+/*
+https://github.com/practicetasks/java_tasks/blob/main/collections/comparisons/task_1/README.md
+ */
+
 public class Practice {
+    public static void main(String[] args) {
+        RentedFilm film1 = new RentedFilm(
+                "Терминатор",
+                new DateTime(20, 11, 2021, 10, 0, 0),
+                new DateTime(27, 11, 2021, 23, 58, 58)
+        );
+        System.out.println("Фильм Терминатор взят в аренду: " + film1.getTimeOfRent());
+        System.out.println("Фильм должен быть вернут до: " + film1.getTimeOfReturn());
+
+
+        DateTime today = new DateTime(27, 11, 2021, 23, 58, 59);
+
+        System.out.println("Сегодняшнее число: " + today);
+
+        DateTimeComparator comparator = new DateTimeComparator();
+        boolean shouldAlreadyBeReturned = comparator.compare(today, film1.getTimeOfReturn()) > 0;
+
+        System.out.println("Прошло ли время возврата? " + (shouldAlreadyBeReturned ? "Да!" : "Нет!"));
+    }
 }
