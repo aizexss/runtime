f5e4e0272a37b4ed28d1e11ae69ff394a3e2c681
stnqq <life33890@gmail.com>
2026-05-24 15:19:03 +0500

    comparator example

--- fourteen_collections/practice/PracticeSeven.java
+++ fourteen_collections/practice/PracticeSeven.java
@@ -5,15 +5,35 @@ import java.util.Collections;
 import java.util.List;
 
 public class PracticeSeven {
+//    public static void main(String[] args) {
+//        List<Long> listOfLongs = new ArrayList<>();
+//        listOfLongs.add(157478403L);
+//        listOfLongs.add(2450033L);
+//        listOfLongs.add(32039458858604L);
+//
+//        System.out.println(listOfLongs);
+//
+//        Collections.fill(listOfLongs, 55555555L);
+//        System.out.println(listOfLongs);
+//    }
+
     public static void main(String[] args) {
-        List<Long> listOfLongs = new ArrayList<>();
-        listOfLongs.add(157478403L);
-        listOfLongs.add(2450033L);
-        listOfLongs.add(32039458858604L);
+        List<Long> listOfSmallLongs = new ArrayList<>();
+        listOfSmallLongs.add(1L);
+        listOfSmallLongs.add(2L);
+        listOfSmallLongs.add(3L);
 
-        System.out.println(listOfLongs);
+        List<Long> listOfBigLongs = new ArrayList<>();
+        listOfBigLongs.add(10000000L);
+        listOfBigLongs.add(20000000L);
+        listOfBigLongs.add(30000000L);
 
-        Collections.fill(listOfLongs, 55555555L);
-        System.out.println(listOfLongs);
+        System.out.println(listOfSmallLongs);
+        System.out.println(listOfBigLongs);
+
+        Collections.copy(listOfSmallLongs, listOfBigLongs);
+
+        System.out.println(listOfSmallLongs);
+        System.out.println(listOfBigLongs);
     }
 }
