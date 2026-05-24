2749d67a478a18a245555fcd28f54a3483e2e2d3
stnqq <life33890@gmail.com>
2026-05-24 15:06:28 +0500

    Collections.fill()

---
+++ fourteen_collections/practice/PracticeSeven.java
@@ -0,0 +1,19 @@
+package fourteen_collections.practice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PracticeSeven {
+    public static void main(String[] args) {
+        List<Long> listOfLongs = new ArrayList<>();
+        listOfLongs.add(157478403L);
+        listOfLongs.add(2450033L);
+        listOfLongs.add(32039458858604L);
+
+        System.out.println(listOfLongs);
+
+        Collections.fill(listOfLongs, 55555555L);
+        System.out.println(listOfLongs);
+    }
+}
